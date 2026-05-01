e9e600b5bbdd2ccb30644bbf1661c6782fcaa595
Omer I.S. <137101815+omeritzics@users.noreply.github.com>
2026-05-01 00:39:24 +0000

    Update download links to point to Updatium

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 window.onload = async () => {
     const dd = document.querySelector('#downloadDiv')
-    const response = await fetch('https://api.github.com/repos/ImranR98/Obtainium/releases/latest');
+    const response = await fetch('https://api.github.com/repos/omeritzics/Updatium/releases/tag/qama-v26.4');
     const tag = (await response.json()).tag_name;
     if (dd) {
         dd.innerHTML = `
-        <a type="button" class="btn btn-warning" href="https://github.com/ImranR98/Obtainium/releases/download/${tag}/app-release.apk">Download Universal APK</a>
-        <a type="button" class="btn btn-outline-warning" href="https://github.com/ImranR98/Obtainium/releases/download/${tag}/app-arm64-v8a-release.apk">armv8</a>
-        <a type="button" class="btn btn-outline-warning" href="https://github.com/ImranR98/Obtainium/releases/download/${tag}/app-armeabi-v7a-release.apk">armv7</a>
-        <a type="button" class="btn btn-outline-warning" href="https://github.com/ImranR98/Obtainium/releases/download/${tag}/app-x86_64-release.apk">x64</a>
+        <a type="button" class="btn btn-warning" href="https://github.com/omeritzics/Updatium/releases/download/qama-v26.4/Updatium-Qama-v26.4.apk">Download</a>
         `
     }
 }
